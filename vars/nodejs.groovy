4b325ae83e0acd3f61b524bed3d5f7e9c22ea113
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-21 23:22:25 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -49,7 +49,7 @@ def call(COMPONENT){
                 steps{
                     sh 'echo prepairing artifacts'
                     sh 'npm clean-install'
-                    sh "zip -r $COMPONENT.zip node_modeles server.js"
+                    //sh "zip -r $COMPONENT.zip node_modeles server.js"
                 }
             }
             stage('Publishing artifacts'){
