3c25c3a902ad06dbc19f11f4739142c26afc01a3
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-22 14:03:04 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -50,8 +50,8 @@ def call(COMPONENT){
                 steps{
                     script{
                         print "checking releases"
-                        def upload_status =sh(returnStdout: true, script: "curl -s -L http://172.31.22.7:8081/service/rest/repository/browse/$COMPONENT/ | grep $TAG_NAME")
-                        print upload_status
+                        #def upload_status =sh(returnStdout: true, script: "curl -s -L http://172.31.22.7:8081/service/rest/repository/browse/$COMPONENT/ | grep $TAG_NAME")
+                        #print upload_status
                     }
                 }
             }            
