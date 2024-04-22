ee19a8b34c032be7489c9f5262733b9a005ef1b5
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-22 14:26:07 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -69,7 +69,7 @@ def call(COMPONENT){
             stage('Publishing artifacts'){
                 when{
                     expression {env.TAG_NAME != null}
-                    expression {env.upload_status == null}}
+                    expression {env.upload_status == null}
                 }
                 steps{
                     sh 'echo Publishing artifacts'
