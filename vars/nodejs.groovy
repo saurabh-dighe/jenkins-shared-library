4d1654915695ba85d465c3ddcb94831030394722
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-22 14:03:40 +0530

    auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -47,13 +47,13 @@ def call(COMPONENT){
                 when{
                     expression {env.TAG_NAME != null}
                 }
-                steps{
-                    script{
-                        print "checking releases"
-                        #def upload_status =sh(returnStdout: true, script: "curl -s -L http://172.31.22.7:8081/service/rest/repository/browse/$COMPONENT/ | grep $TAG_NAME")
-                        #print upload_status
-                    }
-                }
+                // steps{
+                //     script{
+                //         print "checking releases"
+                //         def upload_status =sh(returnStdout: true, script: "curl -s -L http://172.31.22.7:8081/service/rest/repository/browse/$COMPONENT/ | grep $TAG_NAME")
+                //         print upload_status
+                //     }
+                // }
             }            
             stage('Making artifacts'){
                 when{
