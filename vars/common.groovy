fc4acb0bdf9a4e9c7e88d9760f1f1a40b4ae17d1
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-04-23 12:55:11 +0530

    auto push

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,15 @@
-def sonarchecks(){
-    sh "echo Sonar checks started for $COMPONENT"
-    //sh "sonar-scanner -Dsonar.host.url=http://$NEXES_URL:9000 -Dsonar.login=$SONAR_CRED_USR -Dsonar.password=$SONAR_CRED_PSW -Dsonar.projectKey=$COMPONENT $ARGS"
-    sh "echo Sonar checks completd for $COMPONENT"
+def lint-checks(){
+    stage('Lint Checks'){
+        sh "echo performing Lint Checks for $COMPONENT"
+    }
+}
+
+def sonar-checks(){
+    stage('Static Code Analysis'){
+        sh "echo Sonar checks started for $COMPONENT"
+        //sh "sonar-scanner -Dsonar.host.url=http://$NEXES_URL:9000 -Dsonar.login=$SONAR_CRED_USR -Dsonar.password=$SONAR_CRED_PSW -Dsonar.projectKey=$COMPONENT $ARGS"
+        sh "echo Sonar checks completd for $COMPONENT"
+    }
 }
 
 def sonarrusult(){
@@ -10,17 +18,52 @@ def sonarrusult(){
     sh "echo sonar scan completed"
 }
 
-def testcases(){
-    
-    def stages = [:]
-    stages["Unit Testing"] = {
-        echo "Unit testing completed"
+def test-cases(){
+    stage('Running Test Cases'){
+        def stages = [:]
+        stages["Unit Testing"] = {
+            echo "Unit testing completed"
+        }
+        stages["Integration Testing"] = {
+            echo "Integration testing completed"
+        }
+        stages["Functional Testing"] = {
+            echo "Functional testing completed"
+        }
+        parallel(stages)
     }
-    stages["Integration Testing"] = {
-        echo "Integration testing completed"
+}
+
+def artifacts(){
+    stage('Checking Artifacts on Nexus'){
+        env.upload_status = sh(returnStdout: true, script: "curl -s -L http://172.31.22.7:8081/service/rest/repository/browse/$COMPONENT/ | grep $COMOPENT-$TAG_NAME || true")
+        print upload_status
     }
-    stages["Functional Testing"] = {
-        echo "Functional testing completed"
-    }
-    parallel(stages)
+
+    if(env.upload_status == ""){
+        stage('Prepairing Artifacts'){
+            if(env.APPTYPE == "nodejs"){
+                sh 'echo prepairing artifacts'
+                sh 'npm install'
+                sh 'zip -r $COMPONENT-$TAG_NAME.zip node_modules server.js'
+            }
+            else if(env.APPTYPE == "java"){
+                sh 'echo prepairing artifacts'
+                sh 'mvn clean install'
+                sh 'zip -r $COMPONENT-$TAG_NAME.zip node_modules server.js systemd.service'
+            }
+            else if(env.APPTYPE == "python"){
+                sh 'echo prepairing artifacts'
+                sh 'pip3 install'
+                sh 'zip -r $COMPONENT-$TAG_NAME.zip node_modules server.js'
+            }
+        }
+        stage('Publishing Artifacts'){
+            sh 'echo Publishing artifacts'
+            sh 'curl -f -v -u admin:password --upload-file $COMPONENT-$TAG_NAME.zip http://172.31.22.7:8081/repository/$COMPONENT/$COMPONENT-$TAG_NAME.zip'
+        }
+    } 
+    else{
+        sh "echo Arifact $COMOPENT-$TAG_NAME already present"
+    }   
 }
--- vars/java.groovy
+++ vars/java.groovy
@@ -1,69 +1,87 @@
-def lintchecks(){
-    sh "echo performing Lint Checks for $COMPONENT"
-    // sh "mvn checkstyle:check|| true"
-}
-
 def call(COMPONENT){
-    pipeline { 
-        agent {
-            label 'ws'
-        }
-        environment{
-            NEXES_URL= "172.31.19.197"
-            SONAR_CRED= credentials('NEXES_CRED')
-        }
-        stages {
-            stage('Lint Checks') {
-                steps {
-                    script {
-                        lintchecks()
-                    }
-                }
-            }
-            stage('Compiling java code'){
-                steps {
-                    sh "mvn clean compile"
-                    sh "ls -ltr target/"
-                }
-            }            
-            stage('Static Code Analysis'){
-                steps {
-                    script {
-                        env.ARGS = "-Dsonar.java.binaries=target/"
-                        common.sonarchecks()
-                    }
-                }
-            }
-            stage('Get sonar result'){
-                steps {
-                    script {
-                        common.sonarrusult()
-                    }
-                }
-            }
-            stage('Performing testing'){
-                steps{
-                    script{
-                        common.testcases()
-                    }
-                }
-            } 
-            stage('Making artifacts'){
-                when{
-                    expression {env.TAG_NAME != null}
-                }
-                steps{
-                    sh 'echo prepairing artifacts'
-                }
-            }
-            stage('Publishing artifacts'){
-                when{
-                    expression {env.TAG_NAME != null}
-                }
-                steps{
-                    sh 'echo Publishing artifacts'
-                }
-            }          
-       }
+    common.lint-checks()
+    
+    common.sonar-checks()
+
+    common.test-cases()
+
+    if(env.TAG_NAME != null)
+    {
+        common.artifacts() 
     }
 }
+
+
+
+
+
+
+// def lintchecks(){
+//     sh "echo performing Lint Checks for $COMPONENT"
+//     // sh "mvn checkstyle:check|| true"
+// }
+
+// def call(COMPONENT){
+//     pipeline { 
+//         agent {
+//             label 'ws'
+//         }
+//         environment{
+//             NEXES_URL= "172.31.19.197"
+//             SONAR_CRED= credentials('NEXES_CRED')
+//         }
+//         stages {
+//             stage('Lint Checks') {
+//                 steps {
+//                     script {
+//                         lintchecks()
+//                     }
+//                 }
+//             }
+//             stage('Compiling java code'){
+//                 steps {
+//                     sh "mvn clean compile"
+//                     sh "ls -ltr target/"
+//                 }
+//             }            
+//             stage('Static Code Analysis'){
+//                 steps {
+//                     script {
+//                         env.ARGS = "-Dsonar.java.binaries=target/"
+//                         common.sonarchecks()
+//                     }
+//                 }
+//             }
+//             stage('Get sonar result'){
+//                 steps {
+//                     script {
+//                         common.sonarrusult()
+//                     }
+//                 }
+//             }
+//             stage('Performing testing'){
+//                 steps{
+//                     script{
+//                         common.testcases()
+//                     }
+//                 }
+//             } 
+//             stage('Making artifacts'){
+//                 when{
+//                     expression {env.TAG_NAME != null}
+//                 }
+//                 steps{
+//                     sh 'echo prepairing artifacts'
+//                 }
+//             }
+//             stage('Publishing artifacts'){
+//                 when{
+//                     expression {env.TAG_NAME != null}
+//                 }
+//                 steps{
+//                     sh 'echo Publishing artifacts'
+//                 }
+//             }          
+//        }
+//     }
+// }
