4c35c4d90413ae833dfe4c3c62d1d1aa7f112364
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-06-24 13:43:55 +0530

    Auto push

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -7,34 +7,32 @@ def call(){
         // common.testcases()
         if(env.TAG_NAME != null)
         {
-            if(env.upload_status == ""){
-                stage('Generate Docker Image'){
-                    if(env.APPTYPE == "nodejs"){
-                        sh "echo prepairing artifacts"
-                        sh "npm install"
-                    }
-                    else if(env.APPTYPE == "maven"){
-                        sh "echo Preparing Artifacts"
-                        sh "mvn clean package"
-                        sh "mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar"
-                        sh "zip -r ${COMPONENT}-${TAG_NAME}.zip  ${COMPONENT}.jar systemd.service"
-                    }
-                    else if(env.APPTYPE == "python"){
-                        sh "echo preparing artifacts"
-                        sh "zip -r ${COMPONENT}-${TAG_NAME}.zip *.py *.ini requirements.txt"
-                    }
-                    else if(env.APPTYPE == "angular"){
-                        sh '''
-                            echo preparing artifacts
-                            cd static/
-                            zip ../${COMPONENT}-${TAG_NAME}.zip *
-                        '''
-                    }
+            stage('Generate Docker Image'){
+                if(env.APPTYPE == "nodejs"){
+                    sh "npm install"
                 }
-                stage('Publish Docker Image') {
+                else if(env.APPTYPE == "maven"){
+                    sh "mvn clean package"
+                    sh "mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar"
+                    sh "zip -r ${COMPONENT}-${TAG_NAME}.zip  ${COMPONENT}.jar systemd.service"
+                }
+                else if(env.APPTYPE == "python"){
+                    sh "zip -r ${COMPONENT}-${TAG_NAME}.zip *.py *.ini requirements.txt"
+                }
+                else if(env.APPTYPE == "angular"){
+                    sh '''
+                        cd static/
+                        zip ../${COMPONENT}-${TAG_NAME}.zip *
+                    '''
+                }
+            }
+            stage('Publish Docker Image') {
+                sh '''
+                    wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem"
+                    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 992382386864.dkr.ecr.us-east-1.amazonaws.com
                     docker build -t 992382386864.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME} .
                     docker push 992382386864.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}
-                }
+                '''
             }
         }
     }
